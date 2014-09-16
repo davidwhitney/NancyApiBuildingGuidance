c521e5122cee037f11087f183b900ee5fba5b53d
David Whitney <david@davidwhitney.co.uk>
2014-09-16 17:17:49 +0100

    Custom media types.

---
+++ WebApp/Infrastructure/CustomMediaTypes/Application_FlatText/FlatTextResponseProcessor.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using Nancy.Responses.Negotiation;
+
+namespace WebApp.Infrastructure.CustomMediaTypes.Application_FlatText
+{
+    public class FlatTextResponseProcessor : IResponseProcessor
+    {
+        public const string CustomContentType = "application/flat-text";
+        public const string CustomContentExtension = "flat-text";
+        private readonly ISerializer _serializer;
+
+        public FlatTextResponseProcessor(IEnumerable<ISerializer> serializers)
+        {
+            _serializer = serializers.FirstOrDefault(x => x.CanSerialize(CustomContentType));
+        }
+
+        public IEnumerable<Tuple<string, MediaRange>> ExtensionMappings
+        {
+            get { return new[] { new Tuple<string, MediaRange>(CustomContentExtension, new MediaRange(CustomContentType)) }; }
+        }
+
+        public ProcessorMatch CanProcess(MediaRange requestedMediaRange, dynamic model, NancyContext context)
+        {
+            return !requestedMediaRange.Matches(CustomContentType) 
+                ? new ProcessorMatch { ModelResult = MatchResult.DontCare, RequestedContentTypeResult = MatchResult.NoMatch } 
+                : new ProcessorMatch { ModelResult = MatchResult.DontCare, RequestedContentTypeResult = MatchResult.ExactMatch };
+        }
+
+        public Response Process(MediaRange requestedMediaRange, dynamic model, NancyContext context)
+        {
+            return new Response
+            {
+                Contents = stream => _serializer.Serialize(CustomContentType, model, stream),
+                ContentType = CustomContentType,
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+    }
+}
---
+++ WebApp/Infrastructure/CustomMediaTypes/Application_FlatText/FlatTextSerializer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Nancy;
+using Nancy.IO;
+
+namespace WebApp.Infrastructure.CustomMediaTypes.Application_FlatText
+{
+    public class FlatTextSerializer : ISerializer
+    {
+        public IEnumerable<string> Extensions
+        {
+            get { return new[] { FlatTextResponseProcessor .CustomContentExtension }; }
+        }
+
+        public bool CanSerialize(string contentType)
+        {
+            return contentType == FlatTextResponseProcessor.CustomContentType;
+        }
+
+        public void Serialize<TModel>(string contentType, TModel model, Stream outputStream)
+        {
+            using (var writer = new StreamWriter(new UnclosableStreamWrapper(outputStream)))
+            {
+                foreach (var property in model.GetType().GetProperties())
+                {
+                    writer.Write(property.Name + ": " + property.GetValue(model));
+                }
+            }
+        }
+    }
+}
